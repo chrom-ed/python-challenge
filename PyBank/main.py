c8518d0091992f2cf6ee5b7121ccd5d59723c681
Evan Wilson <chromevalkierie.evan@gmail.com>
2018-06-21 22:24:03 -0500

    sums total, but would be inefficient with large datasets

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,13 @@
 # import data file
 import csv
 with open('/Users/Evan/Git/python-challenge/PyBank/budget_data.csv') as csvfile:
+    # create total profit/loss variable
+    total = 0
 
     # iterate through each row of datafile
     fileReader = csv.reader(csvfile)
-    for row in fileReader:
-        print(row)
-
-#
+    for lstRow in fileReader:
+        if lstRow[0] != 'Date':
+            net = lstRow[1]
+            total += int(net)
+print(total)
