fe87eadd2fd1760c000c93a6001e3740dfa98bfe
Evan Wilson <chromevalkierie.evan@gmail.com>
2018-06-23 10:19:45 -0500

    PyBank homework fully functional using inefficient data methods

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,23 +30,41 @@ with open('/Users/Evan/Git/python-challenge/PyBank/budget_data.csv') as csvfile:
                 maxloss = net
                 lossmonth = lstRow[0]
 
+# create output text file
+outputFile = open(
+    '/Users/Evan/Git/python-challenge/PyBank/budget_data_output.txt', 'w')
+
 # begin analysis printout
 print("Financial Analysis")
+outputFile.write("Financial Analysis" + '\n')
 print("--------------------------------------")
+outputFile.write("--------------------------------------" + '\n')
 # print final month total
 print("Total months of data: " + str(totalMonths))
+outputFile.write("Total months of data: " + str(totalMonths) + '\n')
 
 # print sum of profit/loss
 print("Net profit or loss over given period: " +
       "$" + "{:,.2f}".format(totalNet))
+outputFile.write("Net profit or loss over given period: " +
+                 "$" + "{:,.2f}".format(totalNet) + '\n')
 
 # average profit/loss over total months
 average = totalNet / totalMonths
 print("Average change in value over given period: " +
       "$" + "{:,.2f}".format(average))
+outputFile.write("Average change in value over given period: " +
+                 "$" + "{:,.2f}".format(average) + '\n')
 
 # print maximum profits and losses
 print("The greatest increase, or profit, occurred during " +
       maxmonth + " in the amount of " + "$" + "{:,.2f}".format(maxnet))
+outputFile.write("The greatest increase, or profit, occurred during " +
+                 maxmonth + " in the amount of " + "$" + "{:,.2f}".format(maxnet) + '\n')
 print("The greatest decrease, or loss, occurred during " +
       lossmonth + " in the amount of " + "$" + "{:,.2f}".format(maxloss))
+outputFile.write("The greatest decrease, or loss, occurred during " +
+                 lossmonth + " in the amount of " + "$" + "{:,.2f}".format(maxloss) + '\n')
+
+# close output file
+outputFile.close()
