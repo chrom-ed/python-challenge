4bcc2575675ef5ad8e84cd462bc662c428c4e17d
Evan Wilson <chromevalkierie.evan@gmail.com>
2018-06-24 12:37:32 -0500

    PyPoll completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,11 +3,10 @@ import os
 import csv
 
 # define file path
-electionpath = os.path.join("..", "python-challenge",
-                            "PyPoll", "election_data.csv")
+electionpath = os.path.join("..", "python-challenge", "PyPoll")
 
 # import data file
-with open(electionpath) as csvfile:
+with open(electionpath + "/election_data.csv") as csvfile:
     # create dictionary reader
     pollreader = csv.DictReader(csvfile)
 
@@ -23,16 +22,31 @@ with open(electionpath) as csvfile:
             candidates[dctVote['Candidate']] = int(1)
         else:
             candidates[dctVote['Candidate']] += 1
-        print(candidates)
+        if votes % 10000 == 0:
+            print(votes)
 
-        # test break
-        if votes > 10000:
-            break
+            # create output file
+with open(electionpath + '/election_data_output.txt', 'w') as outputfile:
 
+    # initialize winning vote tally
     vmax = 0
+
+    # write file header
+    print("\n-------------------Final Vote Count-------------------\n")
+    outputfile.write(
+        "\n-------------------Final Vote Count-------------------\n")
+
+    # summarize each candidates votes
     for c, v in candidates.items():
-        print(f"{c} received {v / votes * 100}% of the votes with {v} votes")
+        print(
+            f"{c} received:\n {v / votes * 100:,.3f}% of the votes with:\n {v} votes.")
+        outputfile.write(
+            f"{c} received:\n {v / votes * 100:,.3f}% of the votes with:\n {v} votes.")
         if v > vmax:
             vmax = v
             winner = c
+    print("\n-------------------And the Winner is-------------------\n")
+    outputfile.write(
+        "\n-------------------And the Winner is-------------------\n")
     print(f"{winner} is the winner with {vmax} votes!")
+    outputfile.write(f"{winner} is the winner with {vmax} votes!")
